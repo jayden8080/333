bda41c64d934103b076d7c03404672e2d387a3e2
DESKTOP-GC0MR36\kicc <gimjuhan416@gmail.com>
2024-07-30 09:46:40 +0900

    align

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Quest.dart';
+import 'align.dart';
 
 
 void main() {
--- lib/widget.dart
+++ lib/widget.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     style: TextStyle(fontSize: 33),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
